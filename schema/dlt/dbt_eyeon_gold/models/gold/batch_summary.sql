3056d4106f9adb1d1ce399dd6fd11cfdd41c71dd
Grant Johnson <johnson30@llnl.gov>
2026-03-30 09:14:12 -0700

    Fixed table references from literals

--- schema/dlt/dbt_eyeon_gold/models/gold/batch_summary.sql
+++ schema/dlt/dbt_eyeon_gold/models/gold/batch_summary.sql
@@ -4,8 +4,8 @@ select
   count(distinct b._dlt_load_id) num_batches, 
   count(o.*) num_rows, 
   count(distinct md._metadata_table_name) num_md_types,
-  list(distinct md._metadata_table_name) num_md_types
+  list(distinct md._metadata_table_name) md_types
 from {{ ref('stg_batch_info') }} b
 left join {{ ref('stg_raw_obs') }} o on o._dlt_load_id=b._dlt_load_id
-left outer join {{ 'all_metadata'}} md on md.uuid=o.uuid
+left outer join {{ ref('all_metadata') }} md on md.uuid=o.uuid
 group by all order by all
